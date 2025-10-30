f01160defad1fc8fc4b1d8c5e174fdc048f0c813
XPanel Developer <developer@example.com>
2025-10-30 11:48:03 +0800

    恢复完整的Vercel配置

--- api/hello.ts
+++
@@ -1,5 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default (req: VercelRequest, res: VercelResponse) => {
-  res.status(200).json({ message: 'Hello from Vercel!' });
-};
